9ae882354f47993fb154fb2cdf19d84a36d2134f
Gabriel Mullen <gabriel.mullen@snowflake.com>
2022-12-07 18:16:09 -0800

    updated with listing comprehension

--- pages/Connect.py
+++ pages/Connect.py
@@ -28,18 +28,14 @@ def getContext():
     currentwh = session.sql('select current_warehouse()').collect()
     if currentwh[0][0] is None: #need a warehouse to check for roles assigned to the user
         fullroles = session.sql('SHOW ROLES').collect()
-        roles = list()
-        for row in fullroles:
-            roles.append(row[1])
+        roles = [row[1] for row in fullroles]
         st.info('Roles displayed will depend on whether a warehouse was selected. Set Context with a WH and Get Context again for a filtered list of roles associated to your user.')
     else: #get all the roles in Snowflake
         roles = session.sql('select value::STRING ROLE from table(flatten(input => parse_json(current_available_roles())))').to_pandas()
         
     whs = session.sql('SHOW WAREHOUSES').collect()
     fulldbs = session.sql('SHOW DATABASES').collect()
-    dbs = list()
-    for db in fulldbs:
-        dbs.append(db[1])
+    dbs = [db[1] for db in fulldbs]
     
     schemas = session.sql('SHOW SCHEMAS').collect()
     st.session_state.roles = roles
@@ -59,11 +55,7 @@ def getListValues(key):
 def filterSchema():
     if txtDatabase:
         lstSchemas= getListValues('schemas')
-        ret = list()
-        for row in lstSchemas:
-            if row[4] == txtDatabase:
-                ret.append(row[1])
-        return ret
+        return [row[1] for row in lstSchemas if row[4] == txtDatabase]
     else:
         return []
 def setContext():
