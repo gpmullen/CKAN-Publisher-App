446ae2fe475ff41a2f170f3354371772fdcc7f56
Gabriel Mullen <gabriel.mullen@snowflake.com>
2023-03-17 15:38:22 -0700

    Data exchange from other state dpts via Data Share

--- SQL/SP_PUBLISH_DATASET.sql
+++ SQL/SP_PUBLISH_DATASET.sql
@@ -1,25 +1,16 @@
-CREATE OR REPLACE PROCEDURE SP_PUBLISH_DATASET()
+CREATE OR REPLACE PROCEDURE SP_PUBLISH_SPOKE()
 RETURNS VARIANT
 LANGUAGE SQL
-AS
-DECLARE
-    TABLES RESULTSET DEFAULT(select database_name, schema_name, table_name, database_name||'.'||schema_name||'.'||table_name FQTN from control_stream);
-    ret variant default '{}';   
+AS 
 BEGIN
 
-    FOR tbl IN tables DO
-        //drop all published files to internal stage
-        execute immediate ( 'copy into @published_extracts/' ||
-        tbl.table_name || '.csv from ' ||
-        tbl.FQTN || ' SINGLE = TRUE MAX_FILE_SIZE=5368709120 OVERWRITE=TRUE file_format = (TYPE = csv COMPRESSION = none FIELD_OPTIONALLY_ENCLOSED_BY=''\042'');');
-    END FOR;
 
     
     //make the api call. Updates packageid
-    UPDATE CONTROL 
-        set package_id = EXT_PACKAGE_ID 
-    FROM (select            
-      package_create(lower(database_name||'_'||schema_name||'_'||table_name)
+    Insert into published_spoke
+    (package_id,database_name,schema_name, table_name,status) 
+    select            
+    package_create(lower(database_name||'_'||schema_name||'_'||table_name)
                      ,notes
                      ,accesslevel
                      ,contact_name
@@ -30,39 +21,39 @@ BEGIN
       , database_name
       , schema_name
       , table_name
-      , METADATA$ISUPDATE ISUPDATE
-      , METADATA$ACTION ACTION
-      from control_Stream) STRM 
-      
-    WHERE CONTROL.TABLE_NAME = STRM.TABLE_NAME 
-    AND CONTROL.DATABASE_NAME = STRM.DATABASE_NAME
-    AND CONTROL.SCHEMA_NAME = STRM.SCHEMA_NAME
-    AND ISUPDATE = FALSE
-    AND ACTION <> 'DELETE';
+      , 'PACKAGE CREATED'
+      from CONTROL_SPOKE_STREAM      
+    WHERE METADATA$ISUPDATE = FALSE
+    AND METADATA$ACTION <> 'DELETE';
     
 
 
 //This makes another API call which stores the URL as a Resource
-                                                                    
-update control
+                                                         
+update published_spoke
     set resource_id = ext_resource_id
     ,status = 'PUBLISHED'
+    ,last_updated_date = CURRENT_TIMESTAMP()
 FROM (
-    select resource_create(package_id,lower(table_name),notes,'CSV'
-                       ,(get_presigned_url(@published_extracts, table_name || '.csv',604800))) ext_resource_id
-      , package_id
-      , METADATA$ISUPDATE ISUPDATE
-      , METADATA$ACTION ACTION
-                       from control_stream 
-                       WHERE METADATA$ACTION = 'INSERT' 
-                       AND METADATA$ISUPDATE = TRUE) STRM
-WHERE CONTROL.PACKAGE_ID = STRM.PACKAGE_ID
-AND ACTION = 'INSERT'
-AND ISUPDATE = 'TRUE';
+    WITH CS AS (select database_name, schema_name, table_name, notes, presigned_url 
+            FROM OPENDATA_WATERBOARDS_DB.OPENDATA.CONTROL_SPOKE)           
+    select resource_create(ps.package_id,lower(ps.table_name),cs.notes,'CSV'
+                       ,(cs.presigned_url)) ext_resource_id
+      , ps.package_id
 
-insert into ckan_log 
-select current_timestamp(),package_id,table_name 
-from control_Stream;
+    from published_spoke PS
+    INNER JOIN CS
+        on PS.database_name = CS.database_name
+        and PS.schema_name = CS.schema_name
+        and PS.table_name = CS.table_name
+    WHERE ps.status = 'PACKAGE CREATED') SPOKE
+    ;
+
+    set LQID = LAST_QUERY_ID();
+
+
+execute immediate ('insert into ckan_log select current_timestamp(),package_id,table_name from published_spoke BEFORE(STATEMENT => ''' 
+|| $LQID::string || ''') WHERE status = ''PACKAGE CREATED''');
     
 exception
   when other then
@@ -70,6 +61,6 @@ exception
                             'SQLCODE', sqlcode,
                             'SQLERRM', sqlerrm,
                             'SQLSTATE', sqlstate);
-    insert into ckan_log select localtimestamp(), 'ERROR', :err::string from control_Stream;
+    insert into ckan_log select localtimestamp(), 'ERROR', :err::string;
 END;
 
